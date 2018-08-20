770d8b4a7778ba114c7b9ffa54213fd15e035d53
tasleemhalai <tasleembanu.halai@perficient.com>
2018-08-20 13:26:33 -0400

    microservices lab

--- components/movie-fun-api/src/main/java/org/superbiz/moviefun/HomeController.java
+++ components/movie-fun-api/src/main/java/org/superbiz/moviefun/HomeController.java
@@ -1,25 +1,18 @@
 package org.superbiz.moviefun;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.superbiz.moviefun.albums.Album;
-import org.superbiz.moviefun.albums.AlbumFixtures;
-import org.superbiz.moviefun.albums.AlbumsBean;
-import org.superbiz.moviefun.movies.Movie;
-import org.superbiz.moviefun.movies.MovieFixtures;
-import org.superbiz.moviefun.movies.MoviesBean;
-
-import java.util.Map;
 
 @Controller
 public class HomeController {
 
-    private final MoviesBean moviesBean;
+   /* private final MoviesRepository moviesBean;
     private final AlbumsBean albumsBean;
     private final MovieFixtures movieFixtures;
     private final AlbumFixtures albumFixtures;
 
-    public HomeController(MoviesBean moviesBean, AlbumsBean albumsBean, MovieFixtures movieFixtures, AlbumFixtures albumFixtures) {
+    public HomeController(MoviesRepository moviesBean, AlbumsBean albumsBean, MovieFixtures movieFixtures, AlbumFixtures albumFixtures) {
         this.moviesBean = moviesBean;
         this.albumsBean = albumsBean;
         this.movieFixtures = movieFixtures;
@@ -33,8 +26,8 @@ public class HomeController {
 
     @GetMapping("/setup")
     public String setup(Map<String, Object> model) {
-        for (Movie movie : movieFixtures.load()) {
-            moviesBean.addMovie(movie);
+        for (Movie movieInfo : movieFixtures.load()) {
+            moviesBean.addMovie(movieInfo);
         }
 
         for (Album album : albumFixtures.load()) {
@@ -45,5 +38,10 @@ public class HomeController {
         model.put("albums", albumsBean.getAlbums());
 
         return "setup";
+    }*/
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
     }
 }
---
+++ components/movie-fun-api/src/main/java/org/superbiz/moviefun/moviesapi/MovieInfo.java
@@ -0,0 +1,85 @@
+package org.superbiz.moviefun.moviesapi;
+
+import java.util.Objects;
+
+public class MovieInfo {
+
+
+    private long id;
+
+    private String director;
+    private String title;
+    private int year;
+    private String genre;
+    private int rating;
+
+    public MovieInfo(){}
+    public MovieInfo(Long id,String title, String director, String genre, int rating, int year) {
+        this.id=id;
+        this.director = director;
+        this.title = title;
+        this.year = year;
+        this.genre = genre;
+        this.rating = rating;
+    }
+    public String getDirector() {
+        return director;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public long getId() {
+        return id;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        MovieInfo movieInfo = (MovieInfo) o;
+
+        if (id != movieInfo.id) return false;
+        if (year != movieInfo.year) return false;
+        if (rating != movieInfo.rating) return false;
+        if (director != null ? !director.equals(movieInfo.director) : movieInfo.director != null) return false;
+        if (title != null ? !title.equals(movieInfo.title) : movieInfo.title != null) return false;
+        return genre != null ? genre.equals(movieInfo.genre) : movieInfo.genre == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (director != null ? director.hashCode() : 0);
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        result = 31 * result + year;
+        result = 31 * result + (genre != null ? genre.hashCode() : 0);
+        result = 31 * result + rating;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "MovieInfo{" +
+                "id=" + id +
+                ", director='" + director + '\'' +
+                ", title='" + title + '\'' +
+                ", year=" + year +
+                ", genre='" + genre + '\'' +
+                ", rating=" + rating +
+                '}';
+    }
+}
