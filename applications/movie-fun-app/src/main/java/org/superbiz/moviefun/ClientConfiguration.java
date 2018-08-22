0e4ec74c1bdcd7f714cbc50591c50407bb0673cf
tasleemhalai <tasleembanu.halai@perficient.com>
2018-08-22 13:57:27 -0400

    service-discovery lab

--- applications/movie-fun-app/src/main/java/org/superbiz/moviefun/ClientConfiguration.java
+++ applications/movie-fun-app/src/main/java/org/superbiz/moviefun/ClientConfiguration.java
@@ -10,8 +10,8 @@ import org.superbiz.moviefun.moviesapi.MoviesClient;
 @Configuration
 public class ClientConfiguration {
 
-    @Value("${albums.url}") String albumsUrl;
-    @Value("${movies.url}") String moviesUrl;
+    @Value("//album-service/albums") String albumsUrl;
+    @Value("//movie-service/movies") String moviesUrl;
 
     @Bean
     public AlbumsClient albumsClient(RestOperations restOperations) {
